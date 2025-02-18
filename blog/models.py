3e3b336ef518e4f655c5655f5a28a9e6abb226db
MeruOnin <heidariasan04@gmail.com>
2025-02-18 21:46:16 +0330

    no

--- blog/models.py
+++ blog/models.py
@@ -5,22 +5,23 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 class Post(models.Model):
+    # relation
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='user_posts')
+
+    # data
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    slug = models.SlugField(max_length=200)
+
+    # Date
+    published = models.DateTimeField(default=timezone.now)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
 
     class Status(models.TextChoices):
         DRAFT = 'DR', 'draft'
         PUBLISHED = 'PB', 'published'
         REJECTED = 'RJ', 'rejected'
-
-    # relation
-    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='user_posts')
-    #data
-    title = models.CharField(max_length=200)
-    description = models.TextField()
-    slug = models.SlugField(max_length=200)
-    # Date
-    published = models.DateTimeField(default=timezone.now)
-    created = models.DateTimeField(auto_now_add=True)
-    updated = models.DateTimeField(auto_now=True)
     # choices
     status = models.CharField(max_length=2, choices=Status.choices, default=Status.DRAFT)
 
